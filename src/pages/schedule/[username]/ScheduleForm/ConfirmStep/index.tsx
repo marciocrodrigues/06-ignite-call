71e4f23bf831d00b7f34c50188a92bc0328334b5
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-10-07 01:08:38 -0300

    Página de confirmação

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,5 +1,44 @@
-import { CalendarStep } from '../CalendarStep'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
 
-export function ScheduleForm() {
-  return <CalendarStep />
+export function ConfirmStep() {
+  function handleConfirmScheduling() { }
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setemgro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" crossOrigin />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" crossOrigin />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
